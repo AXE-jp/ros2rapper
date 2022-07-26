aceb1bc7d7d7a203a5fdc2b7bd9e5b2e7f1171bf
syouji <syouji@axe.bz>
2022-07-26 15:42:03 +0900

    Add copyright and license notices

--- ip.hpp
+++ ip.hpp
@@ -1,3 +1,22 @@
+/*
+    Copyright © 2021-2022 AXE, Inc. All Rights Reserved.
+
+    This file is part of ROS2rapper.
+
+    ROS2rapper is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ROS2rapper is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with ROS2rapper.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef IP_HPP
 #define IP_HPP
 
