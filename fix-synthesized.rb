8ae6bbe06a7d234888392502bf71bee9c1c88b01
Daiki Matsunaga <daikim@axe.bz>
2023-01-13 21:29:37 +0900

    add x-value replace rule

--- fix-synthesized.rb
+++ fix-synthesized.rb
@@ -5,3 +5,9 @@ ros2_fixed = File.read(ros2_src)
   .gsub(/ros2_payloads #(.*?);/m, "")
   .gsub(/udp_txbuf_q0,/, "udp_txbuf_q0,\n        payloads_address0,\n        payloads_ce0,\n        payloads_we0,\n        payloads_d0,\n        payloads_q0,")
 File.write(ros2_src, ros2_fixed)
+
+Dir.glob('proj_ros2/solution1/syn/verilog/*.v') do |filename|
+  fixed = File.read(filename)
+    .gsub(/bx/, "b0")
+  File.write(filename, fixed)
+end
