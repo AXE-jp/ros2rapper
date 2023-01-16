f9eb58167496cd24ef47577c33b05e93551e850b
Daiki Matsunaga <daikim@axe.bz>
2023-01-16 19:33:35 +0900

    fix ros2 port

--- fix-synthesized.rb
+++ fix-synthesized.rb
@@ -4,6 +4,11 @@ ros2_src = "proj_ros2/solution1/syn/verilog/ros2.v"
 ros2_fixed = File.read(ros2_src)
   .gsub(/ros2_payloads #(.*?);/m, "")
   .gsub(/udp_txbuf_q0,/, "udp_txbuf_q0,\n        payloads_address0,\n        payloads_ce0,\n        payloads_we0,\n        payloads_d0,\n        payloads_q0,")
+  .gsub(/reg.*\[11:0\] payloads_address0;/, "output reg   [11:0] payloads_address0;")
+  .gsub(/reg.*payloads_ce0;/,             "output reg    payloads_ce0;")
+  .gsub(/reg.*payloads_we0;/,             "output reg    payloads_we0;")
+  .gsub(/wire.*\[7:0\] payloads_d0;/,       "output wire   [7:0] payloads_d0;")
+  .gsub(/wire.*\[7:0\] payloads_q0;/,       "input wire   [7:0] payloads_q0;")
 File.write(ros2_src, ros2_fixed)
 
 Dir.glob('proj_ros2/solution1/syn/verilog/*.v') do |filename|
