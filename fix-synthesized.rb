35a61eee181696cea97ac4e4b0fdf1d5660f791c
Daiki Matsunaga <daikim@axe.bz>
2023-01-17 16:40:35 +0900

    remove initial block

--- fix-synthesized.rb
+++ fix-synthesized.rb
@@ -2,6 +2,7 @@
 
 ros2_src = "proj_ros2/solution1/syn/verilog/ros2.v"
 ros2_fixed = File.read(ros2_src)
+  .gsub(/\/\/ power-on initialization(.*?)^end$/m, "")
   .gsub(/ros2_payloads #(.*?);/m, "")
   .gsub(/udp_txbuf_q0,/, "udp_txbuf_q0,\n        payloads_address0,\n        payloads_ce0,\n        payloads_we0,\n        payloads_d0,\n        payloads_q0,")
   .gsub(/reg.*\[11:0\] payloads_address0;/, "output reg   [11:0] payloads_address0;")
