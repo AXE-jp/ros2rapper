158007bc84c5b39ef4f688249c9e418385b1136e
Daiki Matsunaga <daikim@axe.bz>
2023-01-18 19:36:18 +0900

    fix initial statements in all files

--- fix-synthesized.rb
+++ fix-synthesized.rb
@@ -2,7 +2,6 @@
 
 ros2_src = "proj_ros2/solution1/syn/verilog/ros2.v"
 ros2_fixed = File.read(ros2_src)
-  .gsub(/\/\/ power-on initialization(.*?)^end$/m, "")
   .gsub(/ros2_payloads #(.*?);/m, "")
   .gsub(/udp_txbuf_q0,/, "udp_txbuf_q0,\n        payloads_address0,\n        payloads_ce0,\n        payloads_we0,\n        payloads_d0,\n        payloads_q0,")
   .gsub(/reg.*\[11:0\] payloads_address0;/, "output reg   [11:0] payloads_address0;")
@@ -15,5 +14,6 @@ File.write(ros2_src, ros2_fixed)
 Dir.glob('proj_ros2/solution1/syn/verilog/*.v') do |filename|
   fixed = File.read(filename)
     .gsub(/bx/, "b0")
+    .gsub(/\/\/ power-on initialization(.*?)^end$/m, "")
   File.write(filename, fixed)
 end
