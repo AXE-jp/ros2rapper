de262bb1d31cb990280c79590bd26a4e8a079b09
Daiki Matsunaga <daikim@axe.bz>
2023-08-25 22:18:40 +0900

    fix

--- ether-src/ros2_eth_rx_adapter.v
+++ ether-src/ros2_eth_rx_adapter.v
@@ -31,6 +31,19 @@ module ros2_eth_rx_adapter (
   input  wire        i_rx_payload_tstrb
 );
 
+  localparam IP_HDR_SIZE = 20;
+
+  localparam IP_HDR_OFFSET_VERSION_IHL = 0;  // Version, IHL
+  localparam IP_HDR_OFFSET_TOS         = 1;  // Type of Service
+  localparam IP_HDR_OFFSET_TOT_LEN     = 2;  // Total Length
+  localparam IP_HDR_OFFSET_ID          = 4;  // Identification
+  localparam IP_HDR_OFFSET_FLAG_OFF    = 6;  // Flags, Fragment Offset
+  localparam IP_HDR_OFFSET_TTL         = 8;  // Time to Live
+  localparam IP_HDR_OFFSET_PROTOCOL    = 9;  // Protocol
+  localparam IP_HDR_OFFSET_CHECK       = 10; // Header Checksum
+  localparam IP_HDR_OFFSET_SADDR       = 12; // Source Address
+  localparam IP_HDR_OFFSET_DADDR       = 16; // Destination Address
+
   localparam STATE_RX_HDR       = 0;
   localparam STATE_RX_WRITE_HDR = 1;
   localparam STATE_RX_PAYLOAD   = 2;
@@ -38,112 +51,128 @@ module ros2_eth_rx_adapter (
   reg [1:0] state;
   reg [15:0] offset;
 
-  reg [31:0] iphdr_dest_ip;
-  reg [31:0] iphdr_source_ip;
-  reg [15:0] iphdr_header_checksum;
-  reg [7:0]  iphdr_protocol;
-  reg [7:0]  iphdr_ttl;
-  reg [12:0] iphdr_fragment_offset;
-  reg [2:0]  iphdr_flags;
-  reg [15:0] iphdr_identification;
-  reg [15:0] iphdr_length;
-  reg [1:0]  iphdr_ecn;
-  reg [5:0]  iphdr_dscp;
-  reg [3:0]  iphdr_ihl;
   reg [3:0]  iphdr_version;
+  reg [3:0]  iphdr_ihl;
+  reg [5:0]  iphdr_dscp;
+  reg [1:0]  iphdr_ecn;
+  reg [15:0] iphdr_length;
+  reg [15:0] iphdr_identification;
+  reg [2:0]  iphdr_flags;
+  reg [12:0] iphdr_fragment_offset;
+  reg [7:0]  iphdr_ttl;
+  reg [7:0]  iphdr_protocol;
+  reg [15:0] iphdr_header_checksum;
+  reg [31:0] iphdr_source_ip;
+  reg [31:0] iphdr_dest_ip;
 
   reg [7:0] dout_data;
   always @(*) begin
     if (state == STATE_RX_WRITE_HDR) begin
       case (offset)
-      `IP_HDR_OFFSET_VERSION_IHL:
+      IP_HDR_OFFSET_VERSION_IHL:
         dout_data = {iphdr_version, iphdr_ihl};
-      `IP_HDR_OFFSET_TOS:
+      IP_HDR_OFFSET_TOS:
         dout_data = {iphdr_dscp, iphdr_ecn};
-      `IP_HDR_OFFSET_TOT_LEN:
+      IP_HDR_OFFSET_TOT_LEN:
         dout_data = iphdr_length[15:8];
-      `IP_HDR_OFFSET_TOT_LEN + 1:
+      IP_HDR_OFFSET_TOT_LEN + 1:
         dout_data = iphdr_length[7:0];
-      `IP_HDR_OFFSET_ID:
+      IP_HDR_OFFSET_ID:
         dout_data = iphdr_identification[15:8];
-      `IP_HDR_OFFSET_ID + 1:
+      IP_HDR_OFFSET_ID + 1:
         dout_data = iphdr_identification[7:0];
-      `IP_HDR_OFFSET_FLAG_OFF:
+      IP_HDR_OFFSET_FLAG_OFF:
         dout_data = {iphdr_flags, iphdr_fragment_offset[12:8]};
-      `IP_HDR_OFFSET_FLAG_OFF + 1:
+      IP_HDR_OFFSET_FLAG_OFF + 1:
         dout_data = iphdr_fragment_offset[7:0];
-      `IP_HDR_OFFSET_TTL:
+      IP_HDR_OFFSET_TTL:
         dout_data = iphdr_ttl;
-      `IP_HDR_OFFSET_PROTOCOL:
+      IP_HDR_OFFSET_PROTOCOL:
         dout_data = iphdr_protocol;
-      `IP_HDR_OFFSET_CHECK:
+      IP_HDR_OFFSET_CHECK:
         dout_data = iphdr_header_checksum[15:8];
-      `IP_HDR_OFFSET_CHECK + 1:
+      IP_HDR_OFFSET_CHECK + 1:
         dout_data = iphdr_header_checksum[7:0];
-      `IP_HDR_OFFSET_SADDR:
+      IP_HDR_OFFSET_SADDR:
         dout_data = iphdr_source_ip[31:24];
-      `IP_HDR_OFFSET_SADDR + 1:
+      IP_HDR_OFFSET_SADDR + 1:
         dout_data = iphdr_source_ip[23:16];
-      `IP_HDR_OFFSET_SADDR + 2:
+      IP_HDR_OFFSET_SADDR + 2:
         dout_data = iphdr_source_ip[15:8];
-      `IP_HDR_OFFSET_SADDR + 3:
+      IP_HDR_OFFSET_SADDR + 3:
         dout_data = iphdr_source_ip[7:0];
-      `IP_HDR_OFFSET_DADDR:
+      IP_HDR_OFFSET_DADDR:
         dout_data = iphdr_dest_ip[31:24];
-      `IP_HDR_OFFSET_DADDR + 1:
+      IP_HDR_OFFSET_DADDR + 1:
         dout_data = iphdr_dest_ip[23:16];
-      `IP_HDR_OFFSET_DADDR + 2:
+      IP_HDR_OFFSET_DADDR + 2:
         dout_data = iphdr_dest_ip[15:8];
-      `IP_HDR_OFFSET_DADDR + 3:
+      IP_HDR_OFFSET_DADDR + 3:
         dout_data = iphdr_dest_ip[7:0];
+      default:
+        dout_data = 0;
       endcase
     end else begin
       dout_data = i_rx_payload_tdata;
     end
   end
 
-  wire o_rx_hdr_ready = (state == STATE_RX_HDR);
-  wire o_dout_data = dout_data;
-  wire o_dout_valid = (state == STATE_RX_WRITE_HDR) | (state == STATE_RX_PAYLOAD & i_dout_full_n);
+  assign o_rx_hdr_ready = (state == STATE_RX_HDR);
+  assign o_dout_data = dout_data;
+  assign o_dout_wr_en = (state == STATE_RX_WRITE_HDR) | (state == STATE_RX_PAYLOAD & i_rx_payload_tvalid);
+  assign o_rx_payload_tready = (state == STATE_RX_PAYLOAD & i_dout_full_n);
 
   always @(posedge i_clk or negedge i_rst_n) begin
     if (!i_rst_n) begin
       state <= STATE_RX_HDR;
       offset <= 0;
+      iphdr_version <= 0;
+      iphdr_ihl <= 0;
+      iphdr_dscp <= 0;
+      iphdr_ecn <= 0;
+      iphdr_length <= 0;
+      iphdr_identification <= 0;
+      iphdr_flags <= 0;
+      iphdr_fragment_offset <= 0;
+      iphdr_ttl <= 0;
+      iphdr_protocol <= 0;
+      iphdr_header_checksum <= 0;
+      iphdr_source_ip <= 0;
+      iphdr_dest_ip <= 0;
     end else begin
-      if (!enable) begin
+      if (!i_enable) begin
         state <= STATE_RX_HDR;
       end else begin
         case (state)
         STATE_RX_HDR: begin
           if (i_rx_hdr_valid) begin
-            iphdr_dest_ip <= i_rx_ip_dest_ip;
-            iphdr_source_ip <= i_rx_ip_source_ip;
-            iphdr_header_checksum <= i_rx_ip_header_checksum;
-            iphdr_protocol <= i_rx_ip_protocol;
-            iphdr_ttl <= i_rx_ip_ttl;
-            iphdr_fragment_offset <= i_rx_ip_fragment_offset;
-            iphdr_flags <= i_rx_ip_flags;
-            iphdr_identification <= i_rx_ip_identification;
-            iphdr_length <= i_rx_ip_length;
-            iphdr_ecn <= i_rx_ip_ecn;
-            iphdr_dscp <= i_rx_ip_dscp;
-            iphdr_ihl <= i_rx_ip_ihl;
             iphdr_version <= i_rx_ip_version;
+            iphdr_ihl <= i_rx_ip_ihl;
+            iphdr_dscp <= i_rx_ip_dscp;
+            iphdr_ecn <= i_rx_ip_ecn;
+            iphdr_length <= i_rx_ip_length;
+            iphdr_identification <= i_rx_ip_identification;
+            iphdr_flags <= i_rx_ip_flags;
+            iphdr_fragment_offset <= i_rx_ip_fragment_offset;
+            iphdr_ttl <= i_rx_ip_ttl;
+            iphdr_protocol <= i_rx_ip_protocol;
+            iphdr_header_checksum <= i_rx_ip_header_checksum;
+            iphdr_source_ip <= i_rx_ip_source_ip;
+            iphdr_dest_ip <= i_rx_ip_dest_ip;
             offset <= 0;
             state <= STATE_RX_WRITE_HDR;
           end
         end
         STATE_RX_WRITE_HDR: begin
           if (i_dout_full_n) begin
-            if (offset + 1 == `IP_HDR_SIZE)
+            if (offset + 1 == IP_HDR_SIZE)
               state <= STATE_RX_PAYLOAD;
             else
               offset <= offset + 1;
           end
         end
         STATE_RX_PAYLOAD: begin
-          if (o_dout_valid & i_rx_payload_tlast)
+          if (i_rx_payload_tvalid & i_dout_full_n & i_rx_payload_tlast)
             state <= STATE_RX_HDR;
         end
         default: begin
@@ -154,6 +183,44 @@ module ros2_eth_rx_adapter (
       end
     end
   end
+
+`ifdef FORMAL
+    // preparation for using $past
+    reg f_past_valid;
+    initial f_past_valid = 1'b0;
+    always @(posedge i_clk)
+        f_past_valid <= 1'b1;
+
+    // reset
+    initial assume(~i_rst_n);
+    always @(posedge i_clk) begin
+        if (!f_past_valid)
+            assume(~i_rst_n);
+        else begin
+            assume(i_rst_n);
+        end
+    end
+
+    always @(posedge i_clk) begin
+        if (f_past_valid) begin
+            if ($past(~i_rst_n | ~i_enable)) begin
+                state_transition_check_0: assert(state == STATE_RX_HDR);
+            end else begin
+                if ($past(state == STATE_RX_HDR))
+                    state_transition_check_1: assert(state == STATE_RX_HDR | state == STATE_RX_WRITE_HDR);
+                else if ($past(state == STATE_RX_WRITE_HDR))
+                    state_transition_check_2: assert(state == STATE_RX_WRITE_HDR | state == STATE_RX_PAYLOAD);
+                else if ($past(state == STATE_RX_PAYLOAD))
+                    state_transition_check_3: assert(state == STATE_RX_PAYLOAD | state == STATE_RX_HDR);
+
+                assert(i_dout_full_n);
+            end
+        end
+    end
+
+    always @(posedge i_clk) begin
+    end
+`endif
 
 endmodule
 
--- ether-src/ros2_eth_tx_adapter.v
+++ ether-src/ros2_eth_tx_adapter.v
@@ -10,13 +10,13 @@ module ros2_eth_tx_adapter (
   output wire        o_din_rd_en,
   output wire        o_tx_hdr_valid,
   input  wire        i_tx_hdr_ready,
-  output wire [31:0] o_tx_ip_dest_ip,
-  output wire [31:0] o_tx_ip_source_ip,
-  output wire [7:0]  o_tx_ip_protocol,
-  output wire [7:0]  o_tx_ip_ttl,
-  output wire [15:0] o_tx_ip_length,
-  output wire [1:0]  o_tx_ip_ecn,
   output wire [5:0]  o_tx_ip_dscp,
+  output wire [1:0]  o_tx_ip_ecn,
+  output wire [15:0] o_tx_ip_length,
+  output wire [7:0]  o_tx_ip_ttl,
+  output wire [7:0]  o_tx_ip_protocol,
+  output wire [31:0] o_tx_ip_source_ip,
+  output wire [31:0] o_tx_ip_dest_ip,
   output wire        o_tx_payload_tvalid,
   input  wire        i_tx_payload_tready,
   output wire [7:0]  o_tx_payload_tdata,
@@ -25,36 +25,50 @@ module ros2_eth_tx_adapter (
   output wire        o_tx_payload_tstrb
 );
 
+  localparam IP_HDR_SIZE = 20;
+
+  localparam IP_HDR_OFFSET_VERSION_IHL = 0;  // Version, IHL
+  localparam IP_HDR_OFFSET_TOS         = 1;  // Type of Service
+  localparam IP_HDR_OFFSET_TOT_LEN     = 2;  // Total Length
+  localparam IP_HDR_OFFSET_ID          = 4;  // Identification
+  localparam IP_HDR_OFFSET_FLAG_OFF    = 6;  // Flags, Fragment Offset
+  localparam IP_HDR_OFFSET_TTL         = 8;  // Time to Live
+  localparam IP_HDR_OFFSET_PROTOCOL    = 9;  // Protocol
+  localparam IP_HDR_OFFSET_CHECK       = 10; // Header Checksum
+  localparam IP_HDR_OFFSET_SADDR       = 12; // Source Address
+  localparam IP_HDR_OFFSET_DADDR       = 16; // Destination Address
+
   localparam STATE_TX_READ_HDR = 0;
   localparam STATE_TX_HDR      = 1;
   localparam STATE_TX_PAYLOAD  = 2;
 
   reg [1:0] state;
+  reg [15:0] offset;
   reg [15:0] len;
   reg [15:0] counter;
 
-  reg [31:0] iphdr_dest_ip;
-  reg [31:0] iphdr_source_ip;
-  reg [7:0]  iphdr_protocol;
-  reg [7:0]  iphdr_ttl;
-  reg [15:0] iphdr_length;
-  reg [1:0]  iphdr_ecn;
   reg [5:0]  iphdr_dscp;
+  reg [1:0]  iphdr_ecn;
+  reg [15:0] iphdr_length;
+  reg [7:0]  iphdr_ttl;
+  reg [7:0]  iphdr_protocol;
+  reg [31:0] iphdr_source_ip;
+  reg [31:0] iphdr_dest_ip;
 
   assign o_din_rd_en = (state == STATE_TX_READ_HDR) | (state == STATE_TX_PAYLOAD & i_tx_payload_tready);
   assign o_tx_hdr_valid = (state == STATE_TX_HDR);
 
-  assign o_tx_ip_dest_ip = iphdr_dest_ip;
-  assign o_tx_ip_source_ip = iphdr_source_ip;
-  assign o_tx_ip_protocol = iphdr_protocol;
-  assign o_tx_ip_ttl = iphdr_ttl;
-  assign o_tx_ip_length = iphdr_length;
-  assign o_tx_ip_ecn = iphdr_ecn;
   assign o_tx_ip_dscp = iphdr_dscp;
+  assign o_tx_ip_ecn = iphdr_ecn;
+  assign o_tx_ip_length = iphdr_length;
+  assign o_tx_ip_ttl = iphdr_ttl;
+  assign o_tx_ip_protocol = iphdr_protocol;
+  assign o_tx_ip_source_ip = iphdr_source_ip;
+  assign o_tx_ip_dest_ip = iphdr_dest_ip;
 
   assign o_tx_payload_tvalid = (state == STATE_TX_PAYLOAD & i_din_empty_n);
   assign o_tx_payload_tdata = i_din_data;
-  assign o_tx_payload_tlast = (i == len);
+  assign o_tx_payload_tlast = (counter + 1 == len);
   assign o_tx_payload_tkeep = 0;
   assign o_tx_payload_tstrb = 0;
 
@@ -64,51 +78,52 @@ module ros2_eth_tx_adapter (
       offset <= 0;
       len <= 0;
       counter <= 0;
-      iphdr_dest_ip <= 0;
-      iphdr_source_ip <= 0;
-      iphdr_protocol <= 0;
-      iphdr_ttl <= 0;
-      iphdr_length <= 0;
-      iphdr_ecn <= 0;
       iphdr_dscp <= 0;
+      iphdr_ecn <= 0;
+      iphdr_length <= 0;
+      iphdr_ttl <= 0;
+      iphdr_protocol <= 0;
+      iphdr_source_ip <= 0;
+      iphdr_dest_ip <= 0;
     end else begin
-      if (!enable) begin
+      if (!i_enable) begin
         state <= STATE_TX_READ_HDR;
+        offset <= 0;
       end else begin
         case (state)
         STATE_TX_READ_HDR: begin
-          if (i_din_valid) begin
+          if (i_din_empty_n) begin
             case (offset)
-            `IP_HDR_OFFSET_TOS: begin
-              iphdr_dscp <= y[7:2];
-              iphdr_ecn <= y[1:0];
+            IP_HDR_OFFSET_TOS: begin
+              iphdr_dscp <= i_din_data[7:2];
+              iphdr_ecn <= i_din_data[1:0];
             end
-            `IP_HDR_OFFSET_TOT_LEN:
-              iphdr_length[15:8] <= y;
-            `IP_HDR_OFFSET_TOT_LEN + 1:
-              iphdr_length[7:0] <= y;
-            `IP_HDR_OFFSET_TTL:
-              iphdr_ttl <= y;
-            `IP_HDR_OFFSET_PROTOCOL:
-              iphdr_protocol <= y;
-            `IP_HDR_OFFSET_SADDR:
-              iphdr_src_addr[31:24] <= y;
-            `IP_HDR_OFFSET_SADDR + 1:
-              iphdr_src_addr[23:16] <= y;
-            `IP_HDR_OFFSET_SADDR + 2:
-              iphdr_src_addr[15:8] <= y;
-            `IP_HDR_OFFSET_SADDR + 3:
-              iphdr_src_addr[7:0] <= y;
-            `IP_HDR_OFFSET_DADDR:
-              iphdr_dst_addr[31:24] <= y;
-            `IP_HDR_OFFSET_DADDR + 1:
-              iphdr_dst_addr[23:16] <= y;
-            `IP_HDR_OFFSET_DADDR + 2:
-              iphdr_dst_addr[15:8] <= y;
-            `IP_HDR_OFFSET_DADDR + 3:
-              iphdr_dst_addr[7:0] <= y;
+            IP_HDR_OFFSET_TOT_LEN:
+              iphdr_length[15:8] <= i_din_data;
+            IP_HDR_OFFSET_TOT_LEN + 1:
+              iphdr_length[7:0] <= i_din_data;
+            IP_HDR_OFFSET_TTL:
+              iphdr_ttl <= i_din_data;
+            IP_HDR_OFFSET_PROTOCOL:
+              iphdr_protocol <= i_din_data;
+            IP_HDR_OFFSET_SADDR:
+              iphdr_source_ip[31:24] <= i_din_data;
+            IP_HDR_OFFSET_SADDR + 1:
+              iphdr_source_ip[23:16] <= i_din_data;
+            IP_HDR_OFFSET_SADDR + 2:
+              iphdr_source_ip[15:8] <= i_din_data;
+            IP_HDR_OFFSET_SADDR + 3:
+              iphdr_source_ip[7:0] <= i_din_data;
+            IP_HDR_OFFSET_DADDR:
+              iphdr_dest_ip[31:24] <= i_din_data;
+            IP_HDR_OFFSET_DADDR + 1:
+              iphdr_dest_ip[23:16] <= i_din_data;
+            IP_HDR_OFFSET_DADDR + 2:
+              iphdr_dest_ip[15:8] <= i_din_data;
+            IP_HDR_OFFSET_DADDR + 3:
+              iphdr_dest_ip[7:0] <= i_din_data;
             endcase
-            if (offset + 1 == `IP_HDR_SIZE)
+            if (offset + 1 == IP_HDR_SIZE)
               state <= STATE_TX_HDR;
             else
               offset <= offset + 1;
@@ -116,19 +131,25 @@ module ros2_eth_tx_adapter (
         end
         STATE_TX_HDR: begin
           if (i_tx_hdr_ready) begin
-            state <= STATE_TX_PAYLOAD;
+            state <= (iphdr_length == IP_HDR_SIZE) ? STATE_TX_READ_HDR : STATE_TX_PAYLOAD;
             counter <= 0;
-            len <= iphdr_length - `IP_HDR_SIZE;
+            len <= iphdr_length - IP_HDR_SIZE;
+            offset <= 0;
           end
         end
         STATE_TX_PAYLOAD: begin
-          if (i_din_empty_n) begin
+          if (i_din_empty_n & i_tx_payload_tready) begin
             counter <= counter + 1;
+            if (o_tx_payload_tlast) begin
+              state <= STATE_TX_READ_HDR;
+              offset <= 0;
+            end
           end
         end
         default: begin
           // unreachable
           state <= STATE_TX_READ_HDR;
+          offset <= 0;
         end
         endcase
       end
