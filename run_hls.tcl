4c2c43bf969efdea906afdd9e20c208cd8feacf0
Daiki Matsunaga <daikim@axe.bz>
2023-12-26 11:27:46 +0900

    fix about reset

--- run_hls.tcl
+++ run_hls.tcl
@@ -33,7 +33,7 @@ set_top ros2
 open_solution -reset solution1
 
 set_part xc7a100tcsg324-1
-config_rtl -reset state -reset_level low -reset_async
+config_rtl -reset all -reset_level low -reset_async
 create_clock -period 100MHz
 
 csynth_design
