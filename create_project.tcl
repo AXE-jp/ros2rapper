5fed33e959b6dbbdfa02b53345b51737f12b56fb
Daiki Matsunaga <daikim@axe.bz>
2024-01-18 13:44:47 +0900

    fix create project tcl

--- create_project.tcl
+++ create_project.tcl
@@ -1,12 +1,12 @@
 # create_project.tcl  Tcl script for creating project
 
 set     project_directory   [file dirname [info script]]
-set     project_name        "project"
+set     project_name        "ros2rapper-project"
 set     device_part         "xc7a100tcsg324-1"
-set     design_xdc_file [file join $project_directory "constrs" "arty_a7_eth.xdc" ]
+set     design_xdc_file     [ glob ./constrs/* ]
 
 # Create project
-create_project -force $project_name $project_directory
+create_project -force $project_name $project_directory/$project_name
 
 # Set project properties
 set_property "part"               $device_part     [current_project]
@@ -18,6 +18,10 @@ set_property "target_language"    "Verilog"        [current_project]
 if {[string equal [get_filesets -quiet sources_1] ""]} {
     create_fileset -srcset sources_1
 }
+set obj [get_filesets sources_1]
+set_property -name "loop_count" -value "1000" -objects $obj
+set_property -name "verilog_define" -value "TARGET_XILINX=1" -objects $obj
+set_property -name "verilog_version" -value "verilog_2001" -objects $obj
 
 # Create fileset "constrs_1"
 if {[string equal [get_filesets -quiet constrs_1] ""]} {
@@ -30,7 +34,7 @@ if {[string equal [get_filesets -quiet sim_1] ""]} {
 }
 
 # Create run "synth_1" and set property
-set synth_1_flow     "Vivado Synthesis 2021"
+set synth_1_flow     "Vivado Synthesis 2023"
 set synth_1_strategy "Vivado Synthesis Defaults"
 if {[string equal [get_runs -quiet synth_1] ""]} {
     create_run -name synth_1 -flow $synth_1_flow -strategy $synth_1_strategy -constrset constrs_1
@@ -42,7 +46,7 @@ set_property -name "steps.synth_design.args.more options" -value "-verilog_defin
 current_run -synthesis [get_runs synth_1]
 
 # Create run "impl_1" and set property
-set impl_1_flow      "Vivado Implementation 2021"
+set impl_1_flow      "Vivado Implementation 2023"
 set impl_1_strategy  "Vivado Implementation Defaults"
 if {[string equal [get_runs -quiet impl_1] ""]} {
     create_run -name impl_1 -flow $impl_1_flow -strategy $impl_1_strategy -constrset constrs_1 -parent_run synth_1
