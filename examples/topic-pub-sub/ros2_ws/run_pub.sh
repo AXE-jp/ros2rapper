142416331539a7b5200bce51875931d5925a5da4
Daiki Matsunaga <daikim@axe.bz>
2024-01-30 10:33:59 +0900

    add license text

--- examples/topic-pub-sub/ros2_ws/run_pub.sh
+++ examples/topic-pub-sub/ros2_ws/run_pub.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Copyright (c) 2021-2024 AXE, Inc.
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 rosdep install -i --from-path src --rosdistro humble -y
 colcon build --packages-select pub_numstr
 source install/setup.bash
--- examples/topic-pub-sub/run-subscriber.sh
+++ examples/topic-pub-sub/run-subscriber.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Copyright (c) 2021-2024 AXE, Inc.
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 docker run -it --rm --name rossub \
   --net=host \
   -e FASTRTPS_DEFAULT_PROFILES_FILE=/tmp/fastrtps-profile.xml \
