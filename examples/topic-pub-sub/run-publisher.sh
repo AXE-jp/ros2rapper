f8862c3aafc914b3670d7845188009e8182e40db
Daiki Matsunaga <daikim@axe.bz>
2024-02-20 13:45:49 +0900

    remove container name designation to allow multiple run

--- examples/topic-pub-sub/run-publisher.sh
+++ examples/topic-pub-sub/run-publisher.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2021-2024 AXE, Inc.
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-docker run -it --rm --name rospub \
+docker run -it --rm \
   --net=host \
   -e FASTRTPS_DEFAULT_PROFILES_FILE=/tmp/fastrtps-profile.xml \
   -v ./fastrtps-profile.xml:/tmp/fastrtps-profile.xml \
