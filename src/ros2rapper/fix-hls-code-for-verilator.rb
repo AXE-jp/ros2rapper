2191757ab2e02903696069653b9887c60b479dc1
Daiki Matsunaga <daikim@axe.bz>
2024-02-26 17:51:57 +0900

    remove zero delays from Vitis HLS's output for Verilator simulation

--- src/ros2rapper/fix-hls-code-for-verilator.rb
+++ src/ros2rapper/fix-hls-code-for-verilator.rb
@@ -3,10 +3,10 @@
 # Copyright (c) 2021-2024 AXE, Inc.
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-SRCDIR = './_gensrc/'
+SRCDIR = ARGV[0]
 
 # remove zero delay (#0) timing control for Verilator (causes SEGV)
-Dir.glob(SRCDIR+'*.v') do |filename|
+Dir.glob(SRCDIR+'/*.v') do |filename|
   fixed = File.read(filename)
     .gsub(/\/\/ power-on initialization(.*?)^end$/m, "")
   File.write(filename, fixed)
