e6632d4c91b27f3b7ef798070c0ad3fb1e5c5f5f
TANASE, Ayumu <tanase@axe.bz>
2025-06-05 13:24:30 +0900

    Add forgotten initializations.

--- src/ros2rapper/hls/spdp.cpp
+++ src/ros2rapper/hls/spdp.cpp
@@ -178,6 +178,9 @@ void spdp_reader(hls_uint<9> in, sedp_endpoint reader_tbl[SEDP_READER_MAX],
                         reader.builtin_subrd_rd_seqnum = 1;
                         reader.builtin_pubrd_wr_seqnum = 0;
                         reader.builtin_subrd_wr_seqnum = 0;
+                        reader.builtin_pubrd_acknack_req = false;
+                        reader.builtin_subrd_acknack_req = false;
+                        reader.initial_send_counter = 0;
                         reader.pub_heartbeat_cnt = 0;
                         reader.sub_heartbeat_cnt = 0;
                         reader.pub_acknack_cnt = 0;
