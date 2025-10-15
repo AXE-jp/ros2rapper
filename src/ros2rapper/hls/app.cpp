35a5e9993e023a0d9c467d5c3af1f0526dc03e55
TANASE, Ayumu <tanase@axe.bz>
2025-10-15 15:55:46 +0900

    Use hls_stream in app_writer

--- src/ros2rapper/hls/app.cpp
+++ src/ros2rapper/hls/app.cpp
@@ -18,7 +18,8 @@ void app_writer(const uint8_t writer_guid_prefix[12],
                 VOLATILE
 #endif // PUB_DATA_FF
                 const uint32_t app_data[MAX_APP_DATA_LEN / 4],
-                uint32_t app_data_len, uint8_t buf[], timestamp now) {
+                uint32_t app_data_len, hls_stream<uint8_t> &out,
+                timestamp now) {
 #pragma HLS inline
 #ifdef SBM_ENDIAN_LITTLE
     static const uint8_t  sbm_flags = SBM_FLAGS_ENDIANNESS;
@@ -39,82 +40,82 @@ void app_writer(const uint8_t writer_guid_prefix[12],
     int32_t  seqnum_h = seqnum >> 32;
     uint32_t seqnum_l = seqnum & 0xffffffff;
 
-    buf[0] = 'R';
-    buf[1] = 'T';
-    buf[2] = 'P';
-    buf[3] = 'S';
-    buf[4] = RTPS_HDR_PROTOCOL_VERSION >> 8;
-    buf[5] = RTPS_HDR_PROTOCOL_VERSION & 0xff;
-    buf[6] = RTPS_HDR_VENDOR_ID >> 8;
-    buf[7] = RTPS_HDR_VENDOR_ID & 0xff;
-    buf[8] = writer_guid_prefix[0];
-    buf[9] = writer_guid_prefix[1];
-    buf[10] = writer_guid_prefix[2];
-    buf[11] = writer_guid_prefix[3];
-    buf[12] = writer_guid_prefix[4];
-    buf[13] = writer_guid_prefix[5];
-    buf[14] = writer_guid_prefix[6];
-    buf[15] = writer_guid_prefix[7];
-    buf[16] = writer_guid_prefix[8];
-    buf[17] = writer_guid_prefix[9];
-    buf[18] = writer_guid_prefix[10];
-    buf[19] = writer_guid_prefix[11];
-    buf[20] = SBM_ID_INFO_DST;
-    buf[21] = sbm_flags;
-    buf[22] = S_BYTE0(GUID_PREFIX_SIZE);
-    buf[23] = S_BYTE1(GUID_PREFIX_SIZE);
-    buf[24] = reader_guid_prefix[0];
-    buf[25] = reader_guid_prefix[1];
-    buf[26] = reader_guid_prefix[2];
-    buf[27] = reader_guid_prefix[3];
-    buf[28] = reader_guid_prefix[4];
-    buf[29] = reader_guid_prefix[5];
-    buf[30] = reader_guid_prefix[6];
-    buf[31] = reader_guid_prefix[7];
-    buf[32] = reader_guid_prefix[8];
-    buf[33] = reader_guid_prefix[9];
-    buf[34] = reader_guid_prefix[10];
-    buf[35] = reader_guid_prefix[11];
-    buf[36] = SBM_ID_INFO_TS;
-    buf[37] = sbm_flags;
-    buf[38] = S_BYTE0(TIMESTAMP_SIZE);
-    buf[39] = S_BYTE1(TIMESTAMP_SIZE);
-    buf[40] = L_BYTE0(now.seconds);
-    buf[41] = L_BYTE1(now.seconds);
-    buf[42] = L_BYTE2(now.seconds);
-    buf[43] = L_BYTE3(now.seconds);
-    buf[44] = L_BYTE0(now.fraction);
-    buf[45] = L_BYTE1(now.fraction);
-    buf[46] = L_BYTE2(now.fraction);
-    buf[47] = L_BYTE3(now.fraction);
-    buf[48] = SBM_ID_DATA;
-    buf[49] = sbm_flags | SBM_FLAGS_DATA;
-    buf[50] = S_BYTE0(octets_to_next_header);
-    buf[51] = S_BYTE1(octets_to_next_header);
-    buf[52] = ext_flags >> 8;
-    buf[53] = ext_flags & 0xff;
-    buf[54] = S_BYTE0(SBM_DATA_HDR_OCTETS_TO_INLINE_QOS);
-    buf[55] = S_BYTE1(SBM_DATA_HDR_OCTETS_TO_INLINE_QOS);
-    buf[56] = reader_entity_id[0];
-    buf[57] = reader_entity_id[1];
-    buf[58] = reader_entity_id[2];
-    buf[59] = reader_entity_id[3];
-    buf[60] = writer_entity_id[0];
-    buf[61] = writer_entity_id[1];
-    buf[62] = writer_entity_id[2];
-    buf[63] = writer_entity_id[3];
-    buf[64] = L_BYTE0(seqnum_h);
-    buf[65] = L_BYTE1(seqnum_h);
-    buf[66] = L_BYTE2(seqnum_h);
-    buf[67] = L_BYTE3(seqnum_h);
-    buf[68] = L_BYTE0(seqnum_l);
-    buf[69] = L_BYTE1(seqnum_l);
-    buf[70] = L_BYTE2(seqnum_l);
-    buf[71] = L_BYTE3(seqnum_l);
-    buf[72] = rep_id >> 8;
-    buf[73] = rep_id & 0xff;
-    buf[74] = rep_opt >> 8;
-    buf[75] = rep_opt & 0xff;
+    out.write('R');
+    out.write('T');
+    out.write('P');
+    out.write('S');
+    out.write(RTPS_HDR_PROTOCOL_VERSION >> 8);
+    out.write(RTPS_HDR_PROTOCOL_VERSION & 0xff);
+    out.write(RTPS_HDR_VENDOR_ID >> 8);
+    out.write(RTPS_HDR_VENDOR_ID & 0xff);
+    out.write(writer_guid_prefix[0]);
+    out.write(writer_guid_prefix[1]);
+    out.write(writer_guid_prefix[2]);
+    out.write(writer_guid_prefix[3]);
+    out.write(writer_guid_prefix[4]);
+    out.write(writer_guid_prefix[5]);
+    out.write(writer_guid_prefix[6]);
+    out.write(writer_guid_prefix[7]);
+    out.write(writer_guid_prefix[8]);
+    out.write(writer_guid_prefix[9]);
+    out.write(writer_guid_prefix[10]);
+    out.write(writer_guid_prefix[11]);
+    out.write(SBM_ID_INFO_DST);
+    out.write(sbm_flags);
+    out.write(S_BYTE0(GUID_PREFIX_SIZE));
+    out.write(S_BYTE1(GUID_PREFIX_SIZE));
+    out.write(reader_guid_prefix[0]);
+    out.write(reader_guid_prefix[1]);
+    out.write(reader_guid_prefix[2]);
+    out.write(reader_guid_prefix[3]);
+    out.write(reader_guid_prefix[4]);
+    out.write(reader_guid_prefix[5]);
+    out.write(reader_guid_prefix[6]);
+    out.write(reader_guid_prefix[7]);
+    out.write(reader_guid_prefix[8]);
+    out.write(reader_guid_prefix[9]);
+    out.write(reader_guid_prefix[10]);
+    out.write(reader_guid_prefix[11]);
+    out.write(SBM_ID_INFO_TS);
+    out.write(sbm_flags);
+    out.write(S_BYTE0(TIMESTAMP_SIZE));
+    out.write(S_BYTE1(TIMESTAMP_SIZE));
+    out.write(L_BYTE0(now.seconds));
+    out.write(L_BYTE1(now.seconds));
+    out.write(L_BYTE2(now.seconds));
+    out.write(L_BYTE3(now.seconds));
+    out.write(L_BYTE0(now.fraction));
+    out.write(L_BYTE1(now.fraction));
+    out.write(L_BYTE2(now.fraction));
+    out.write(L_BYTE3(now.fraction));
+    out.write(SBM_ID_DATA);
+    out.write(sbm_flags | SBM_FLAGS_DATA);
+    out.write(S_BYTE0(octets_to_next_header));
+    out.write(S_BYTE1(octets_to_next_header));
+    out.write(ext_flags >> 8);
+    out.write(ext_flags & 0xff);
+    out.write(S_BYTE0(SBM_DATA_HDR_OCTETS_TO_INLINE_QOS));
+    out.write(S_BYTE1(SBM_DATA_HDR_OCTETS_TO_INLINE_QOS));
+    out.write(reader_entity_id[0]);
+    out.write(reader_entity_id[1]);
+    out.write(reader_entity_id[2]);
+    out.write(reader_entity_id[3]);
+    out.write(writer_entity_id[0]);
+    out.write(writer_entity_id[1]);
+    out.write(writer_entity_id[2]);
+    out.write(writer_entity_id[3]);
+    out.write(L_BYTE0(seqnum_h));
+    out.write(L_BYTE1(seqnum_h));
+    out.write(L_BYTE2(seqnum_h));
+    out.write(L_BYTE3(seqnum_h));
+    out.write(L_BYTE0(seqnum_l));
+    out.write(L_BYTE1(seqnum_l));
+    out.write(L_BYTE2(seqnum_l));
+    out.write(L_BYTE3(seqnum_l));
+    out.write(rep_id >> 8);
+    out.write(rep_id & 0xff);
+    out.write(rep_opt >> 8);
+    out.write(rep_opt & 0xff);
 
 #ifdef PUB_DATA_FF
     /* Cyber unroll_times=all */
@@ -122,18 +123,18 @@ void app_writer(const uint8_t writer_guid_prefix[12],
     for (auto j = 0; j < (MAX_APP_DATA_LEN / 4); j++) {
 #ifdef PUB_DATA_FF
 #pragma HLS unroll
+#else // !PUB_DATA_FF
+#pragma HLS pipeline II = 4
 #endif // PUB_DATA_FF
-#ifdef PUB_DATA_RAM
-#pragma HLS pipeline II = 2
-#endif // PUB_DATA_RAM
+        if ((4 * j) >= app_data_len) {
+            break;
+        }
         uint32_t data = app_data[j];
-        buf[APP_HDR_SIZE + (4 * j)] = (data & 0xff);
-        buf[APP_HDR_SIZE + (4 * j) + 1] = ((data >> 8) & 0xff);
-        buf[APP_HDR_SIZE + (4 * j) + 2] = ((data >> 16) & 0xff);
-        buf[APP_HDR_SIZE + (4 * j) + 3] = (data >> 24);
+        out.write(data & 0xff);
+        out.write((data >> 8) & 0xff);
+        out.write((data >> 16) & 0xff);
+        out.write(data >> 24);
     }
-
-    clear_txbuf(buf, APP_HDR_SIZE + MAX_APP_DATA_LEN, MAX_TX_UDP_PAYLOAD_LEN);
 }
 
 enum {
