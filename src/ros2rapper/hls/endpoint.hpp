77188dea05944d0d39fe06b80350c957210ef5a5
TANASE, Ayumu <tanase@axe.bz>
2025-10-31 18:46:32 +0900

    Expand tables

--- src/ros2rapper/hls/endpoint.hpp
+++ src/ros2rapper/hls/endpoint.hpp
@@ -7,25 +7,25 @@
 #include "hls.hpp"
 #include <cstdint>
 
-#define SEDP_READER_MAX 4
-#define APP_READER_MAX  4
+#define SEDP_READER_MAX 1024
+#define APP_READER_MAX  128
 
 #define PUB_TOPICS_MAX 4
 #define SUB_TOPICS_MAX 4
 
-typedef hls_uint<3> sedp_reader_id_t;
-typedef hls_uint<3> app_reader_id_t;
+typedef hls_uint<11> sedp_reader_id_t;
+typedef hls_uint<7> app_reader_id_t;
 typedef hls_uint<2> topic_id_t;
 
 // There is a variable "unused_reader_id" of type sedp_reader_id_t in
 // spdp_reader. "unused_reader_id" becomes SEDP_READER_MAX when sedp_reader_tbl
 // is full.
-static_assert(SEDP_READER_MAX <= 7,
+static_assert(SEDP_READER_MAX <= 2047,
               "sedp_reader_id_t should be able to represent SEDP_READER_MAX.");
 // There is a variable "unused_app_reader_id" of type app_reader_id_t in
 // sedp_reader. "unused_app_reader_id" becomes APP_READER_MAX when
 // app_reader_tbl is full.
-static_assert(APP_READER_MAX <= 7,
+static_assert(APP_READER_MAX <= 255,
               "app_reader_id_t should be able to represent APP_READER_MAX.");
 static_assert((PUB_TOPICS_MAX <= 4) && (SUB_TOPICS_MAX <= 4),
               "topic_id_t should be able to represent PUB_TOPICS_MAX - 1 and "
