c1e244afc43d810b764e1f93183b6b50a0403db4
TANASE, Ayumu <tanase@axe.bz>
2025-11-06 12:19:07 +0900

    Change the width of sedp_reader_tbl RAM from 32 bits to 64 bits

--- src/ros2rapper/hls/endpoint.hpp
+++ src/ros2rapper/hls/endpoint.hpp
@@ -31,56 +31,60 @@ static_assert((PUB_TOPICS_MAX <= 4) && (SUB_TOPICS_MAX <= 4),
               "topic_id_t should be able to represent PUB_TOPICS_MAX - 1 and "
               "SUB_TOPICS_MAX - 1.");
 
+#define SEDP_ENDPOINT_ALIVE             1
 #define SEDP_ENDPOINT_PUBRD_ACKNACK_REQ 2
 #define SEDP_ENDPOINT_SUBRD_ACKNACK_REQ 4
 
 struct sedp_endpoint {
-    // Word 0
-    //  0         alive
-    //  1         builtin_pubrd_acknack_req
-    //  2         builtin_subrd_acknack_req
-    //  8, 9      initial_send_counter
-    // 15 ... 31  udp_port
+    // Word 0, bit 0
     bool        alive;
+    // Word 0, bit 1
     bool        builtin_pubrd_acknack_req;
+    // Word 0, bit 2
     bool        builtin_subrd_acknack_req;
+    // Word 0, bit 8 and 9
     hls_uint<2> initial_send_counter;
+    // Word 0, bit 16 ... 31
     uint8_t     udp_port[2] /* Cyber array=EXPAND, array_index=const */;
+    // Word 0, bit 32 ... 63
     // Word 1
-    uint8_t     builtin_pubrd_wr_seqnum;
-    uint8_t     builtin_pubrd_rd_seqnum;
-    uint8_t     builtin_subrd_wr_seqnum;
-    uint8_t     builtin_subrd_rd_seqnum;
-    // 2
-    uint8_t     ip_addr[4] /* Cyber array=EXPAND, array_index=const */;
-    // 3, 4, 5
     uint8_t     guid_prefix[12] /* Cyber array=EXPAND, array_index=const */;
-    // 6, 7
+    // Word 2, bit 0 ... 31
+    uint8_t     ip_addr[4] /* Cyber array=EXPAND, array_index=const */;
+    // Word 2, bit 32 ... 39
+    uint8_t     builtin_pubrd_wr_seqnum;
+    // Word 2, bit 40 ... 47
+    uint8_t     builtin_pubrd_rd_seqnum;
+    // Word 2, bit 48 ... 55
+    uint8_t     builtin_subrd_wr_seqnum;
+    // Word 2, bit 56 ... 63
+    uint8_t     builtin_subrd_rd_seqnum;
+    // Word 3
     int64_t     builtin_pubwr_lastsn;
-    // 8, 9
+    // Word 4
     int64_t     builtin_subwr_lastsn;
-    // 10
+    // Word 5, bit 0 ... 31
     uint32_t    pub_heartbeat_cnt;
-    // 11
+    // Word 5, bit 32 ... 63
     uint32_t    sub_heartbeat_cnt;
-    // 12
+    // Word 6, bit 0 ... 31
     uint32_t    pub_acknack_cnt;
-    // 13
+    // Word 6, bit 32 ... 63
     uint32_t    sub_acknack_cnt;
-    // 14, 15
+    // Word 7
     int64_t     lease_duration;
-    // 16, 17
+    // Word 8
     int64_t     timestamp;
-    // 18, 19, 20, 21
+    // Word 9 and 10
     bool children[APP_READER_MAX] /* Cyber array=EXPAND, array_index=const */;
 };
 
-static_assert(
-    APP_READER_MAX == 128,
-    "You have to modify sedp_reader_tbl_t, get_sedp_reader_tbl_children and "
-    "set_sedp_reader_tbl_children when you change APP_READER_MAX.");
+static_assert(APP_READER_MAX == 128,
+              "You have to modify sedp_reader_tbl_t, "
+              "get_sedp_reader_tbl_children, set_sedp_reader_tbl_children and "
+              "clear_sedp_reader_tbl_children when you change APP_READER_MAX.");
 typedef struct {
-    uint32_t ram[22 * SEDP_READER_MAX];
+    uint64_t ram[11 * SEDP_READER_MAX];
 } sedp_reader_tbl_t;
 
 using builtin_ep_type_t = hls_uint<2>;
