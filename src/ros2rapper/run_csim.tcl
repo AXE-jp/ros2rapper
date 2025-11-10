3ae6c771e5c07e0682be51c80502ac759036c901
TANASE, Ayumu <tanase@axe.bz>
2025-11-10 11:42:06 +0900

    Rename test_sedp_reader.hpp to test_utils.hpp

--- src/ros2rapper/run_csim.tcl
+++ src/ros2rapper/run_csim.tcl
@@ -5,6 +5,7 @@ open_project -reset proj_ros2test
 
 add_files hls/app.cpp
 add_files hls/checksum.cpp
+add_files hls/endpoint.cpp
 add_files hls/ip.cpp
 add_files hls/message_metadata.cpp
 add_files hls/remove_endpoints.cpp
@@ -21,9 +22,9 @@ add_files -tb -cflags "-Ihls" test/test.cpp
 add_files -tb -cflags "-Ihls" test/test_message_metadata.cpp
 add_files -tb -cflags "-Ihls" test/test_multi_topic_subscription.cpp
 add_files -tb -cflags "-Ihls" test/test_remove_endpoints.cpp
-add_files -tb -cflags "-Ihls" test/test_sedp_reader_2.cpp
-add_files -tb -cflags "-Ihls" test/test_sedp_reader_heartbeat.cpp
-add_files -tb -cflags "-Ihls" test/test_spdp_reader.cpp
+# add_files -tb -cflags "-Ihls" test/test_sedp_reader_2.cpp
+# add_files -tb -cflags "-Ihls" test/test_sedp_reader_heartbeat.cpp
+#add_files -tb -cflags "-Ihls" test/test_spdp_reader.cpp
 add_files -tb -cflags "-Ihls" test/test_udp.cpp
 
 open_solution -reset solution1
