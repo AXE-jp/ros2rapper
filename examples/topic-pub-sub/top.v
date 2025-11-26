76bb803fa82ffa407b33941525826fa8c9b2f005
TANASE, Ayumu <tanase@axe.bz>
2025-11-26 19:50:28 +0900

    Update examples

--- examples/topic-pub-sub/top.v
+++ examples/topic-pub-sub/top.v
@@ -412,6 +412,13 @@ module top (
         .ip_payloadsmem_wdata(payloadsmem_wdata),
         .ip_payloadsmem_rdata(payloadsmem_rdata),
 
+        .tx_raw_eth_data_addr(),
+        .tx_raw_eth_data_ce(),
+        .tx_raw_eth_data_rdata(32'd0),
+        .tx_raw_eth_data_len(0),
+        .tx_raw_eth_kick(1'b0),
+        .tx_raw_eth_complete(),
+
         .arp_req_retry_count(ARP_REQUEST_RETRY_COUNT),
         .arp_req_retry_interval(ARP_REQUEST_RETRY_INTERVAL),
         .arp_req_timeout(ARP_REQUEST_TIMEOUT)
