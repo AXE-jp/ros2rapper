ed9907871cb385281579933bdc3b1ba5cd74533e
Daiki Matsunaga <daikim@axe.bz>
2025-11-28 11:14:47 +0900

    fix rtl ports

--- src/ether/rtl/ros2_ether.v
+++ src/ether/rtl/ros2_ether.v
@@ -161,6 +161,10 @@ module ros2_ether #(
     output wire ros2_sub_app_data_rep_id_3_valid,
     output wire [15:0] ros2_sub_app_data_rep_id_3,
 
+    output wire [63:0] ros2_sub_app_data_recvmsginfo_out_din,
+    input  wire ros2_sub_app_data_recvmsginfo_out_full_n,
+    output wire ros2_sub_app_data_recvmsginfo_out_write,
+
     input  wire [`ROS2_SUB_TOPICS_MAX-1:0] ros2_sub_app_data_req,
     input  wire [`ROS2_SUB_TOPICS_MAX-1:0] ros2_sub_app_data_rel,
     output wire [`ROS2_SUB_TOPICS_MAX-1:0] ros2_sub_app_data_grant,
@@ -506,6 +510,10 @@ ros2rapper (
     .ros2_sub_app_data_len_3(ros2_sub_app_data_len_3),
     .ros2_sub_app_data_rep_id_3_valid(ros2_sub_app_data_rep_id_3_valid),
     .ros2_sub_app_data_rep_id_3(ros2_sub_app_data_rep_id_3),
+
+    .ros2_sub_app_data_recvmsginfo_out_din(ros2_sub_app_data_recvmsginfo_out_din),
+    .ros2_sub_app_data_recvmsginfo_out_full_n(ros2_sub_app_data_recvmsginfo_out_full_n),
+    .ros2_sub_app_data_recvmsginfo_out_write(ros2_sub_app_data_recvmsginfo_out_write),
 
     .ros2_sub_app_data_req(ros2_sub_app_data_req),
     .ros2_sub_app_data_rel(ros2_sub_app_data_rel),
