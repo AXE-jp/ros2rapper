f9ce9afce0a7bc9b12255869c24b23570ae30c8f
Daiki Matsunaga <daikim@axe.bz>
2025-11-29 13:32:27 +0900

    add au25p build scripts

---
+++ src/ros2rapper/run_hls_au25p.tcl
@@ -0,0 +1,23 @@
+# Copyright (c) 2021-2024 AXE, Inc.
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+open_project -reset proj_ros2_main_au25p
+
+add_files hls/endpoint.cpp
+add_files hls/message_metadata.cpp
+add_files hls/remove_endpoints.cpp
+add_files hls/ros2.cpp
+add_files hls/spdp.cpp
+
+set_top ros2_main
+
+open_solution -reset solution1
+
+set_part xcau25p-ffvb676-1-e
+config_rtl -reset all -reset_level low -reset_async
+create_clock -period 100MHz
+
+csynth_design
+#export_design -format ip_catalog
+
+exit
---
+++ src/ros2rapper/run_hls_receiver_au25p.tcl
@@ -0,0 +1,28 @@
+# Copyright (c) 2021-2024 AXE, Inc.
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+open_project -reset proj_ros2_receiver_au25p
+
+add_files hls/app.cpp
+add_files hls/checksum.cpp
+add_files hls/ip.cpp
+add_files hls/remove_endpoints.cpp
+add_files hls/ros2_receiver.cpp
+add_files hls/rtps.cpp
+add_files hls/sedp.cpp
+add_files hls/slip.cpp
+add_files hls/spdp.cpp
+add_files hls/udp.cpp
+
+set_top ros2_receiver
+
+open_solution -reset solution1
+
+set_part xcau25p-ffvb676-1-e
+config_rtl -reset all -reset_level low -reset_async
+create_clock -period 100MHz
+
+csynth_design
+#export_design -format ip_catalog
+
+exit
