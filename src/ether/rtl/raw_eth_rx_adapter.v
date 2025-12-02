de651878b4d035a745d820c1be57a4ce00299e4a
TANASE, Ayumu <tanase@axe.bz>
2025-12-02 20:11:57 +0900

    Change memory format, remove SENDING_SHORT_PACKET_XX and user SENDING_MEM_DATA_XX instead, and add limited broadcast

--- src/ether/rtl/raw_eth_rx_adapter.v
+++ src/ether/rtl/raw_eth_rx_adapter.v
@@ -30,37 +30,23 @@ module raw_eth_rx_adapter (
     localparam [4:0] READING                 = 5'd1; // Read the first 46 bytes to identify the type of the receiving packet
     localparam [4:0] IDENTIFY_PACKET_TYPE    = 5'd2;
     localparam [4:0] SENDING_STREAM          = 5'd3; // Send the remaining packet data
-    localparam [4:0] SENDING_SHORT_PACKET_00 = 5'd4; // Send data_mem data (if the packet length <= 46)
-    localparam [4:0] SENDING_SHORT_PACKET_01 = 5'd5;
-    localparam [4:0] SENDING_SHORT_PACKET_02 = 5'd6;
-    localparam [4:0] SENDING_SHORT_PACKET_03 = 5'd7;
-    localparam [4:0] SENDING_SHORT_PACKET_04 = 5'd8;
-    localparam [4:0] SENDING_SHORT_PACKET_05 = 5'd9;
-    localparam [4:0] SENDING_SHORT_PACKET_06 = 5'd10;
-    localparam [4:0] SENDING_SHORT_PACKET_07 = 5'd11;
-    localparam [4:0] SENDING_SHORT_PACKET_08 = 5'd12;
-    localparam [4:0] SENDING_SHORT_PACKET_09 = 5'd13;
-    localparam [4:0] SENDING_SHORT_PACKET_10 = 5'd14;
-    localparam [4:0] SENDING_SHORT_PACKET_11 = 5'd15;
-    localparam [4:0] COUNT_ONLY              = 5'd16; // When the packet length exceeds `ROS2_MAX_RAW_ETH_MAX_DATA_LEN
-    localparam [4:0] FINISH                  = 5'd17; // Set the packet length and assert frame_ready
-    localparam [4:0] WAIT_LAST               = 5'd18; // Ignore the receiving packet
-    // 19 is not used.
-    localparam [4:0] SENDING_MEM_DATA_00     = 5'd20; // Send data_men data (if the packet length > 46)
-    localparam [4:0] SENDING_MEM_DATA_01     = 5'd21;
-    localparam [4:0] SENDING_MEM_DATA_02     = 5'd22;
-    localparam [4:0] SENDING_MEM_DATA_03     = 5'd23;
-    localparam [4:0] SENDING_MEM_DATA_04     = 5'd24;
-    localparam [4:0] SENDING_MEM_DATA_05     = 5'd25;
-    localparam [4:0] SENDING_MEM_DATA_06     = 5'd26;
-    localparam [4:0] SENDING_MEM_DATA_07     = 5'd27;
-    localparam [4:0] SENDING_MEM_DATA_08     = 5'd28;
-    localparam [4:0] SENDING_MEM_DATA_09     = 5'd29;
-    localparam [4:0] SENDING_MEM_DATA_10     = 5'd30;
-    localparam [4:0] SENDING_MEM_DATA_11     = 5'd31;
+    localparam [4:0] COUNT_ONLY              = 5'd4; // When the packet length exceeds `ROS2_MAX_RAW_ETH_MAX_DATA_LEN - 2
+    localparam [4:0] SENDING_MEM_DATA_00     = 5'd5; // Send data_mem data
+    localparam [4:0] SENDING_MEM_DATA_01     = 5'd6;
+    localparam [4:0] SENDING_MEM_DATA_02     = 5'd7;
+    localparam [4:0] SENDING_MEM_DATA_03     = 5'd8;
+    localparam [4:0] SENDING_MEM_DATA_04     = 5'd9;
+    localparam [4:0] SENDING_MEM_DATA_05     = 5'd10;
+    localparam [4:0] SENDING_MEM_DATA_06     = 5'd11;
+    localparam [4:0] SENDING_MEM_DATA_07     = 5'd12;
+    localparam [4:0] SENDING_MEM_DATA_08     = 5'd13;
+    localparam [4:0] SENDING_MEM_DATA_09     = 5'd14;
+    localparam [4:0] SENDING_MEM_DATA_10     = 5'd15;
+    localparam [4:0] SENDING_MEM_DATA_11     = 5'd16; // Finish write packet data and become IDLE next
+    localparam [4:0] WAIT_LAST               = 5'd17; // Ignore the receiving packet
 
     reg  frame_ready_reg;
-    wire frame_ready_next = (frame_ready_reg & ~rx_raw_eth_data_ack) | (state_reg == FINISH);
+    wire frame_ready_next = (frame_ready_reg & ~rx_raw_eth_data_ack) | (state_reg == SENDING_MEM_DATA_11);
     assign rx_raw_eth_data_frame_ready = frame_ready_reg;
 
     reg axis_tready_reg;
@@ -125,24 +111,27 @@ module raw_eth_rx_adapter (
     assign broadcast_mask[31:24] = subnet_mask[31:24] | ~data_mem[33];
     wire is_unicast_to_me = (unicast_mask == 32'd0);
     wire is_multicast = (data_mem[30][7:4] == 4'b1110);
-    wire is_broadcast = ((unicast_mask & broadcast_mask) == 32'd0);
+    wire is_direct_broadcast = ((unicast_mask & broadcast_mask) == 32'd0);
+    wire is_limited_broadcast = (data_mem[30] == 8'hff) & (data_mem[31] == 8'hff) & (data_mem[32] == 8'hff) & (data_mem[33] == 8'hff);
+    wire is_ipv4_to_me = is_ipv4 & (is_unicast_to_me | is_multicast | is_direct_broadcast | is_limited_broadcast);
     
     wire more_fragment = data_mem[20][5];
     wire fragment_offset = {data_mem[20][4:0], data_mem[21]};
     wire is_fragment = is_ipv4 & (more_fragment | (fragment_offset != 13'd0));
     wire is_udp = (data_mem[23] == 8'd17);
     wire has_rtps_magic = (data_mem[42] == "R") & (data_mem[43] == "T") & (data_mem[44] == "P") & (data_mem[45] == "S");
-    wire is_rtps = is_ipv4 & no_header_option & (is_unicast_to_me | is_multicast | is_broadcast) & is_udp & has_rtps_magic;
+    wire is_rtps = is_ipv4_to_me & no_header_option & is_udp & has_rtps_magic;
 
     localparam ADDR_WIDTH = $clog2(`ROS2_MAX_RAW_ETH_RX_DATA_LEN) - 2;
-    reg [ADDR_WIDTH-1:0] ram_addr_reg;
-    reg [ADDR_WIDTH-1:0] ram_addr_next;
-    reg ram_ce_reg;
-    reg ram_ce_next;
-    reg [3:0] ram_we_reg;
-    reg [3:0] ram_we_next;
-    reg [31:0] ram_wdata_reg;
-    reg [31:0] ram_wdata_next;
+    wire [ADDR_WIDTH+1:0] ram_byte_addr = count_reg + 2'd2; // Shift by the packet length size
+    reg  [ADDR_WIDTH-1:0] ram_addr_reg;
+    reg  [ADDR_WIDTH-1:0] ram_addr_next;
+    reg  ram_ce_reg;
+    reg  ram_ce_next;
+    reg  [3:0] ram_we_reg;
+    reg  [3:0] ram_we_next;
+    reg  [31:0] ram_wdata_reg;
+    reg  [31:0] ram_wdata_next;
     assign rx_raw_eth_data_addr  = ram_addr_reg;
     assign rx_raw_eth_data_ce    = ram_ce_reg;
     assign rx_raw_eth_data_we    = ram_we_reg;
@@ -152,40 +141,35 @@ module raw_eth_rx_adapter (
         ram_ce_next = 1'b0;
         ram_we_next = 4'd0;
         ram_wdata_next = 32'd0;
-        for (iter = 0; iter < 11; iter = iter + 1) begin
-            if ((state_reg == (SENDING_SHORT_PACKET_00 + iter))
-                    || (state_reg == (SENDING_MEM_DATA_00 + iter))) begin
-                ram_addr_next = iter + 1;
-                ram_ce_next = 1'b1;
-                ram_we_next = 4'b1111;
-                ram_wdata_next[7:0]   = data_mem[4*iter];
-                ram_wdata_next[15:8]  = data_mem[4*iter+1];
-                ram_wdata_next[23:16] = data_mem[4*iter+2];
-                ram_wdata_next[31:24] = data_mem[4*iter+3];
-            end
-        end
-        if ((state_reg == SENDING_SHORT_PACKET_11)
-                || (state_reg == SENDING_MEM_DATA_11)) begin
-            ram_addr_next = 12;
-            ram_ce_next = 1'b1;
-            ram_we_next = 4'b0011;
-            ram_wdata_next[7:0]   = data_mem[44];
-            ram_wdata_next[15:8]  = data_mem[45];
-        end else if (state_reg == SENDING_STREAM) begin
-            ram_addr_next = count_reg[COUNT_WIDTH-1:2] + 1'b1;
-            ram_ce_next = 1'b1;
-            case (count_reg[1:0])
+        if (state_reg == SENDING_STREAM) begin
+            ram_addr_next = ram_byte_addr[ADDR_WIDTH+1:2];
+            ram_ce_next = rx_raw_eth_axis_tvalid;
+            case (ram_byte_addr[1:0])
                 2'd0: ram_we_next = 4'b0001;
                 2'd1: ram_we_next = 4'b0010;
                 2'd2: ram_we_next = 4'b0100;
                 2'd3: ram_we_next = 4'b1000;
             endcase
             ram_wdata_next = {4{rx_raw_eth_axis_tdata}};
-        end else if (state_reg == FINISH) begin
-            ram_addr_next = {ADDR_WIDTH{1'b0}};
+        end else if (state_reg == SENDING_MEM_DATA_00) begin
+            ram_addr_next = 0;
             ram_ce_next = 1'b1;
-            ram_we_next = 4'b0011;
+            ram_we_next = 4'b1111;
+            // First two bytes are the packet length
             ram_wdata_next[15:0] = count_reg;
+            ram_wdata_next[23:16] = data_mem[0];
+            ram_wdata_next[31:24] = data_mem[1];
+        end
+        for (iter = 1; iter < 12; iter = iter + 1) begin
+            if (state_reg == (SENDING_MEM_DATA_00 + iter)) begin
+                ram_addr_next = iter + 1;
+                ram_ce_next = 1'b1;
+                ram_we_next = 4'b1111;
+                ram_wdata_next[7:0]   = data_mem[4*iter - 2];
+                ram_wdata_next[15:8]  = data_mem[4*iter - 1];
+                ram_wdata_next[23:16] = data_mem[4*iter];
+                ram_wdata_next[31:24] = data_mem[4*iter + 1];
+            end
         end
     end
     
@@ -205,7 +189,7 @@ module raw_eth_rx_adapter (
                 end else begin
                     count_next = 1;
                     if (rx_raw_eth_axis_tlast) begin
-                        state_next = SENDING_SHORT_PACKET_00;
+                        state_next = SENDING_MEM_DATA_00;
                     end else begin
                         state_next = READING;
                     end
@@ -216,13 +200,13 @@ module raw_eth_rx_adapter (
                 // If state_reg is READING, axis_tready_reg should be 1'b1.
                 count_next = count_reg + 1'b1;
                 if (rx_raw_eth_axis_tlast) begin
-                    state_next = SENDING_SHORT_PACKET_00;
+                    state_next = SENDING_MEM_DATA_00;
                 end else if (count_next == DATA_MEM_SIZE) begin
                     state_next = IDENTIFY_PACKET_TYPE;
                 end
             end
         end else if (state_reg == IDENTIFY_PACKET_TYPE) begin
-            if (!is_arp || !is_ipv4 || is_fragment || !is_rtps) begin
+            if (!is_arp || !is_ipv4_to_me || is_fragment || !is_rtps) begin
                 state_next = SENDING_STREAM;
             end
         end else if (state_reg == SENDING_STREAM) begin
@@ -231,8 +215,8 @@ module raw_eth_rx_adapter (
                 count_next = count_reg + 1'b1;
                 if (rx_raw_eth_axis_tlast) begin
                     state_next = SENDING_MEM_DATA_00;
-                end else if (count_next == (`ROS2_MAX_RAW_ETH_RX_DATA_LEN - 4)) begin
-                    // The packet is too long, so count length only.
+                end else if (count_next == (`ROS2_MAX_RAW_ETH_RX_DATA_LEN - 2)) begin
+                    // The packet is too long, so count the packet length only.
                     state_next = COUNT_ONLY;
                 end
             end
@@ -244,21 +228,19 @@ module raw_eth_rx_adapter (
                     state_next = SENDING_MEM_DATA_00;
                 end
             end
-        end else if (state_reg == FINISH) begin
-            count_next = {COUNT_WIDTH{1'b0}};
-            state_next = IDLE;
         end else if (state_reg == WAIT_LAST) begin
             if (rx_raw_eth_axis_tvalid & rx_raw_eth_axis_tlast) begin
                 // If state_reg is WAIT_LAST, axis_tready_reg should be 1'b1.
                 count_next = {COUNT_WIDTH{1'b0}};
                 state_next = IDLE;
             end
-        end else if ((state_reg == SENDING_SHORT_PACKET_11) || (state_reg == SENDING_MEM_DATA_11)) begin
-            state_next = FINISH;
+        end else if (state_reg == SENDING_MEM_DATA_11) begin
+            count_next = {COUNT_WIDTH{1'b0}};
+            state_next = IDLE;
+
         end
         for (iter = 0; iter < 11; iter = iter + 1) begin
-            if ((state_reg == (SENDING_SHORT_PACKET_00 + iter))
-                    || (state_reg == (SENDING_MEM_DATA_00 + iter))) begin
+            if (state_reg == (SENDING_MEM_DATA_00 + iter)) begin
                 state_next = state_reg + 1'b1;
             end
         end
