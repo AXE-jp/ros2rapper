176c6d18351a63a35ad1cfa5abd19d1bb20d2cd7
TANASE, Ayumu <tanase@axe.bz>
2025-12-24 12:07:41 +0900

    Update examples/raw-eth-tx-rx
    Send UDP packets from raw ether ports to unicast, multicast, direct broadcast and limited broadcast addresses

--- examples/raw-eth-tx-rx/top.v
+++ examples/raw-eth-tx-rx/top.v
@@ -253,67 +253,6 @@ module top (
     );
 `endif
 
-    // Raw Ether TX message
-    wire [47:0] dest_mac_addr = 48'hff_ff_ff_ff_ff_ff; // broadcast
-    wire [31:0] dest_ip_addr  = {8'd2, 8'd1, 8'd168, 8'd192};
-    wire [15:0] udp_src_port  = 16'd1111;
-    wire [15:0] udp_dest_port = 16'd1234;
-
-    // Ether header
-    wire [15:0] eth_type = {8'h00, 8'h08}; // IPv4
-    wire [111:0] raw_eth_hdr = {eth_type, mac_addr, dest_mac_addr};
-
-    // IP header
-    wire [7:0]  ip_ver_ihl = 8'h45;
-    wire [7:0]  ip_tos = 8'h00;
-    wire [15:0] ip_total_length = 16'd44;
-    wire [15:0] ip_identification = 16'd0;
-    wire [7:0]  ip_flags = 8'h40; // Don't fragment
-    wire [7:0]  ip_fragment_offset = 8'd0;
-    wire [7:0]  ip_ttl = 8'd64;
-    wire [7:0]  ip_protocol = 8'd17; // UDP
-    wire [15:0] ip_checksum;
-    wire [159:0] raw_eth_ip_hdr = {
-        dest_ip_addr, ip_addr, ip_checksum, ip_protocol, ip_ttl, ip_fragment_offset, ip_flags,
-        ip_identification[7:0], ip_identification[15:8], ip_total_length[7:0], ip_total_length[15:8], ip_tos, ip_ver_ihl
-        };
-
-    wire [31:0] ip_checksum_0 =
-        raw_eth_ip_hdr[15:0] + raw_eth_ip_hdr[31:16] + raw_eth_ip_hdr[47:32] + raw_eth_ip_hdr[63:48] + raw_eth_ip_hdr[79:64]
-        + raw_eth_ip_hdr[111:96] + raw_eth_ip_hdr[127:112] + raw_eth_ip_hdr[143:128] + raw_eth_ip_hdr[159:144];
-    wire [31:0] ip_checksum_1 = ip_checksum_0[15:0] + ip_checksum_0[31:16];
-    assign ip_checksum = ~(ip_checksum_1[15:0] + ip_checksum_1[31:16]);
-
-    // UDP
-    wire [15:0] udp_length = 16'd24;
-    wire [15:0] udp_checksum = 16'd0; // No checksum
-    wire [127:0] udp_payload = {8'h00, "\ntset rehte war"};
-    wire [191:0] raw_eth_udp_packet = {
-        udp_payload, udp_checksum[7:0], udp_checksum[15:8], udp_length[7:0], udp_length[15:8],
-        udp_dest_port[7:0], udp_dest_port[15:8], udp_src_port[7:0], udp_src_port[15:8]
-        };
-
-    wire [463:0] raw_eth_packet = {raw_eth_udp_packet, raw_eth_ip_hdr, raw_eth_hdr};
-    wire [15:0]  raw_eth_packet_len = 58;
-    wire [479:0] tx_raw_eth_data = {raw_eth_packet, raw_eth_packet_len};
-    wire [$clog2(`ROS2_MAX_RAW_ETH_TX_DATA_LEN)-3:0] tx_raw_eth_data_addr;
-    wire tx_raw_eth_data_ce;
-    reg  [31:0] tx_raw_eth_data_rdata;
-
-    always @(posedge clk_int or negedge rst_n_int) begin
-        if (!rst_n_int) begin
-            tx_raw_eth_data_rdata <= 32'd0;
-        end else begin
-            if (tx_raw_eth_data_ce) begin
-                if (tx_raw_eth_data_addr < 15) begin
-                    tx_raw_eth_data_rdata <= tx_raw_eth_data[32*tx_raw_eth_data_addr +: 32];
-                end else begin
-                    tx_raw_eth_data_rdata <= 32'd0;
-                end
-            end
-        end
-    end
-
     // Raw Ether TX handshake
     reg  [26:0] count;
     wire tx_raw_eth_frame_ready = count[26];
@@ -330,16 +269,136 @@ module top (
         end
     end
 
-    reg  tx_raw_eth_frame_ready_before;
+    reg tx_raw_eth_completed_before;
     always @(posedge clk_int or negedge rst_n_int) begin
         if (!rst_n_int) begin
-            tx_raw_eth_frame_ready_before <= 1'b0;
+            tx_raw_eth_completed_before <= 1'b0;
         end else begin
-            tx_raw_eth_frame_ready_before <= tx_raw_eth_frame_ready;
+            tx_raw_eth_completed_before <= tx_raw_eth_completed_before;
         end
     end
-    // The rising edge of tx_raw_eth_frame_ready
-    wire tx_raw_eth_kick = ~tx_raw_eth_frame_ready_before & tx_raw_eth_frame_ready;
+    // The rising edge of tx_raw_eth_completed
+    wire tx_raw_eth_change_message = ~tx_raw_eth_completed_before & tx_raw_eth_completed;
+
+    // Raw Ether TX IPv4 messages
+    wire [48:0] dest_mac_addr = 48'd0; // Use the ARP module
+    wire [31:0] dest_unicast_ip_addr = {8'd2, 8'd1, 8'd168, 8'd192};
+    wire [31:0] dest_multicast_ip_addr = {8'd1, 8'd1, 8'd1, 8'd239};
+    wire [31:0] dest_direct_broadcast_ip_addr = ip_addr | ~subnet_mask;
+    wire [31:0] dest_limited_broadcast_ip_addr = 32'hff_ff_ff_ff;
+    wire [15:0] udp_src_port  = 16'd1111;
+    wire [15:0] udp_dest_port = 16'd1234;
+
+    // Raw ether header
+    wire [15:0] eth_type = {8'h00, 8'h08}; // IPv4
+    wire [111:0] raw_eth_hdr = {eth_type, mac_addr, dest_mac_addr};
+
+    // Raw ether IP header
+    wire [7:0]  ip_ver_ihl = 8'h45;
+    wire [7:0]  ip_tos = 8'h00;
+    wire [15:0] ip_length;
+    wire [15:0] ip_identification = 16'd0;
+    wire [15:0] ip_fragment_params = 8'h00_40; // Don't fragment and fragment offset is 0
+    wire [7:0]  ip_ttl = 8'd64;
+    wire [7:0]  ip_protocol = 8'd17; // UDP
+    wire [15:0] ip_checksum = 16'd0; // calculated in ROS2rapper
+    reg  [31:0] dest_ip_addr;
+    wire [159:0] raw_eth_ip_hdr = {
+        dest_ip_addr, ip_addr, ip_checksum, ip_protocol, ip_ttl, ip_fragment_offset, ip_flags,
+        ip_identification[7:0], ip_identification[15:8], ip_length[7:0], ip_length[15:8], ip_tos, ip_ver_ihl
+    };
+
+    // Raw ether UDP header
+    reg  [15:0] udp_length;
+    wire [15:0] udp_checksum = 16'd0; // No checksum
+    wire [63:0] raw_eth_udp_header = {
+        udp_checksum[7:0], udp_checksum[15:8], udp_length[7:0], udp_length[15:8],
+        udp_dest_port[7:0], udp_dest_port[15:8], udp_src_port[7:0], udp_src_port[15:8]
+    };
+
+    // Raw ether UDP payload
+    //wire [8*1472-1:0] raw_eth_unicast_udp_payload = {8'h00, "\n", 1470{8'h55}};
+    wire [159:0] raw_eth_multicast_udp_payload = {24'd00, "\n1 tset rehte waR"};
+    wire [159:0] raw_eth_direct_broadcast_udp_payload = {24'd00, "\n2 tset rehte waR"};
+    wire [159:0] raw_eth_limited_broadcast_udp_payload = {24'd00, "\n3 tset rehte waR"};
+    wire [15:0]  raw_eth_unicast_udp_length = 16'd1480;
+    wire [15:0]  raw_eth_multicast_udp_length = 16'd26;
+    wire [15:0]  raw_eth_direct_broadcast_udp_payload = 16'd26;
+    wire [15:0]  raw_eth_limited_broadcast_udp_payload = 16'd26;
+
+    // Select raw ether IP packets
+    localparam [1:0] RAW_ETH_IP_UNICAST           = 2'd0;
+    localparam [1:0] RAW_ETH_IP_MULTICAST         = 2'd1;
+    localparam [1:0] RAW_ETH_IP_DIRECT_BROADCAST  = 2'd2;
+    localparam [1:0] RAW_ETH_IP_LIMITED_BROADCAST = 2'd3;
+    reg [1:0] raw_eth_state;
+    always @(posedge clk_int or negedge rst_n_int) begin
+        if (!rst_n_int) begin
+            raw_eth_state <= RAW_ETH_IP_UNICAST;
+        end else begin
+            if (tx_raw_eth_change_message) begin
+                raw_eth_state <= raw_eth_state + 1'b1;
+            end
+        end
+    end
+
+    // TX raw ether data length and the IP address
+    always @* begin
+        dest_ip_addr = 32'd0;
+        udp_length = 16'd0;
+        if (raw_eth_state == RAW_ETH_IP_UNICAST) begin
+            dest_ip_addr = dest_unicast_ip_addr;
+            udp_length = raw_eth_unicast_udp_length;
+        end else if (raw_eth_state == RAW_ETH_IP_MULTICAST) begin
+            dest_ip_addr = dest_multicast_ip_addr;
+            udp_length = raw_eth_multicast_udp_length;
+        end else if (raw_eth_state == RAW_ETH_IP_DIRECT_BROADCAST) begin
+            dest_ip_addr = dest_direct_broadcast_ip_addr;
+            udp_length = raw_eth_direct_broadcast_udp_length;
+        end else if (raw_eth_state == RAW_ETH_IP_LIMITED_BROADCAST) begin
+            dest_ip_addr = dest_limited_braodcast_ip_addr;
+            udp_length = raw_eth_limited_broadcast_udp_length;
+        end
+    end
+    assign ip_length = udp_length + 16'd20;
+    wire [15:0] tx_raw_eth_data_len = ip_length + 16'd14;
+
+    // TX raw ether memory port
+    wire [$clog2(`ROS2_MAX_RAW_ETH_TX_DATA_LEN)-3:0] tx_raw_eth_data_addr;
+    wire tx_raw_eth_data_ce;
+    reg  [31:0] tx_raw_eth_data_rdata;
+
+    wire [351:0] tx_raw_eth_data_hdr = {raw_eth_udp_hdr, raw_eth_ip_hdr, raw_eth_hdr, tx_raw_eth_data_len};
+    always @(posedge clk_int or negedge rst_n_int) begin
+        if (!rst_n_int) begin
+            tx_raw_eth_data_rdata <= 32'd0;
+        end else begin
+            if (tx_raw_eth_data_ce) begin
+                tx_raw_eth_data_rdata <= 32'd0;
+                if (tx_raw_eth_data_addr < 11) begin
+                    tx_raw_eth_data_rdata <= tx_raw_eth_data_hdr[32*tx_raw_eth_data_addr +: 32];
+                end else if (tx_raw_eth_data_addr < 16) begin
+                    if (raw_eth_state == RAW_ETH_IP_UNICAST) begin
+                        tx_raw_eth_data_rdata <= 32'h55_55_55_55; // UUUU
+                    end else if (raw_eth_state == RAW_ETH_IP_MULTICAST) begin
+                        tx_raw_eth_data_rdata <= raw_eth_multicast_udp_payload[32*(tx_raw_eth_data_addr - 11) +: 32];
+                    end else if (raw_eth_state == RAW_ETH_IP_DIRECT_BROADCAST) begin
+                        tx_raw_eth_data_rdata <= raw_eth_direct_broadcast_udp_payload[32*(tx_raw_eth_data_addr - 11) +: 32];
+                    end else if (raw_eth_state == RAW_ETH_IP_LIMITED_BROADCAST) begin
+                        tx_raw_eth_data_rdata <= raw_eth_limited_broadcast_udp_payload[32*(tx_raw_eth_data_addr - 11) +: 32];
+                    end
+                end else if (tx_raw_eth_data_addr < 378) begin
+                    if (raw_eth_state == RAW_ETH_IP_UNICAST) begin
+                        tx_raw_eth_data_rdata <= 32'h55_55_55_55; // UUUU
+                    end
+                end else if (tx_raw_eth_data_addr == 378) begin
+                    if (raw_eth_state == RAW_ETH_IP_UNICAST) begin
+                        tx_raw_eth_data_rdata <= {8'h00, "\n", 16'h55_55};
+                    end
+                end
+            end
+        end
+    end
 
     // Raw Ether RX
     wire [$clog2(`ROS2_MAX_RAW_ETH_RX_DATA_LEN)-3:0] rx_raw_eth_data_addr;
