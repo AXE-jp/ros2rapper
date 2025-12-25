bc15731eedb6937c04ec99a86b10c83ccded8ed0
TANASE, Ayumu <tanase@axe.bz>
2025-12-25 10:26:07 +0900

    Fix bug

--- examples/raw-eth-tx-rx/ros2_ws/receive_udp.py
+++ examples/raw-eth-tx-rx/ros2_ws/receive_udp.py
@@ -11,16 +11,16 @@ def receive_udp(multicast_addr, port):
     """
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as s:
         # Allow the port reuse
-        s.setsockpot(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         # Receive broadcast packets
         s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         s.bind(("0.0.0.0", port))
         # Join the multicast group
-        mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+        mreq = struct.pack("4sl", socket.inet_aton(multicast_addr), socket.INADDR_ANY)
+        s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
         # Receive UDP packets
         while True:
-            data, addr = sock.recvfrom(2048)
+            data, addr = s.recvfrom(2048)
             print(data.decode("utf-8"))
 
 
