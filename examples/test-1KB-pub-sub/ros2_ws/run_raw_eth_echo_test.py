557a74111923701b7cc47fec00353312f42b3f1d
TANASE, Ayumu <tanase@axe.bz>
2025-12-25 10:13:16 +0900

    Test the echo back from ROS2rapper four times to increase the test coverage

--- examples/test-1KB-pub-sub/ros2_ws/run_raw_eth_echo_test.py
+++ examples/test-1KB-pub-sub/ros2_ws/run_raw_eth_echo_test.py
@@ -25,19 +25,19 @@ if __name__ == "__main__":
     src_mac = mac_to_bin(get_mac(ifname))
     dst_mac = mac_to_bin("02:00:00:00:00:00")
     eth_type = eth_type_u16.to_bytes(2, byteorder="big")
-    payload = random.randbytes(1500)
 
     with socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(eth_type_u16)) as s:
         s.bind((ifname, eth_type_u16))
-        s.send(dst_mac + src_mac + eth_type + payload)
-        received = s.recv(2048)
-        dst_is_ok = (received[0:6] == src_mac)
-        src_is_ok = (received[6:12] == dst_mac)
-        type_is_ok = (received[12:14] == eth_type)
-        payload_is_ok = (received[14:] == payload)
-
-        if dst_is_ok and src_is_ok and type_is_ok and payload_is_ok:
-            print("OK")
-        else:
-            print("Bad response")
+        for length in (1500, 1499, 1498, 1497):
+            payload = random.randbytes(length)
+            s.send(dst_mac + src_mac + eth_type + payload)
+            received = s.recv(2048)
+            dst_is_ok = (received[0:6] == src_mac)
+            src_is_ok = (received[6:12] == dst_mac)
+            type_is_ok = (received[12:14] == eth_type)
+            payload_is_ok = (received[14:] == payload)
+            if dst_is_ok and src_is_ok and type_is_ok and payload_is_ok:
+                print("OK")
+            else:
+                print("Bad response")
 
