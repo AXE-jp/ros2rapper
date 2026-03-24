6882fb61f3e8b3b9c6331ad7d272ac2ab86ce020
TANASE, Ayumu <tanase@axe.bz>
2026-03-24 13:20:20 +0900

    Remove sedp_reader from sedp.[hc]pp

--- src/ros2rapper/hls/sedp.hpp
+++ src/ros2rapper/hls/sedp.hpp
@@ -38,20 +38,6 @@
 #define SEDP_ACKNACK_UDP_PKT_LEN  (UDP_HDR_SIZE + SEDP_ACKNACK_RTPS_PKT_LEN)
 #define SEDP_ACKNACK_IP_PKT_LEN   (IP_HDR_SIZE + SEDP_ACKNACK_UDP_PKT_LEN)
 
-void sedp_reader(
-    hls_uint<9> in, hls_stream<rtps_data_t> &out,
-    hls_uint<PUB_TOPICS_MAX> pub_enable, hls_uint<SUB_TOPICS_MAX> sub_enable,
-    const uint8_t ip_addr[4], const uint8_t subnet_mask[4],
-    uint16_t port_num_seed, const uint8_t guid_prefix[12],
-    const uint8_t pub_topic_name[PUB_TOPICS_MAX][MAX_TOPIC_NAME_LEN],
-    const uint8_t pub_topic_name_len[PUB_TOPICS_MAX],
-    const uint8_t pub_type_name[PUB_TOPICS_MAX][MAX_TOPIC_TYPE_NAME_LEN],
-    const uint8_t pub_type_name_len[PUB_TOPICS_MAX],
-    const uint8_t sub_topic_name[SUB_TOPICS_MAX][MAX_TOPIC_NAME_LEN],
-    const uint8_t sub_topic_name_len[SUB_TOPICS_MAX],
-    const uint8_t sub_type_name[SUB_TOPICS_MAX][MAX_TOPIC_TYPE_NAME_LEN],
-    const uint8_t sub_type_name_len[SUB_TOPICS_MAX]);
-
 void sedp_writer(
     const uint8_t vendor_id[2], const uint8_t writer_guid_prefix[12],
     const uint8_t writer_entity_id[4], const uint8_t reader_guid_prefix[12],
