c59eaafca574c04be83f631e0821d18dd4991186
TANASE, Ayumu <tanase@axe.bz>
2026-03-27 10:32:35 +0900

    Update copyright messages

--- examples/topic-pub-sub/test.sv
+++ examples/topic-pub-sub/test.sv
@@ -1,3 +1,6 @@
+// Copyright (c) 2021-2026 AXE, Inc.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 `resetall
 `default_nettype wire
 `timescale 1ns / 1ps
--- src/ether/run_tb.tcl
+++ src/ether/run_tb.tcl
@@ -1,4 +1,4 @@
-# Copyright (c) 2021-2024 AXE, Inc.
+# Copyright (c) 2021-2026 AXE, Inc.
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # run_tb.tcl  Tcl script for running testbench
